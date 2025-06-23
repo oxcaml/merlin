519d470c652d6e47e0a058876e136367e4bcb989
Liam Stevenson <lstevenson@janestreet.com>
2025-06-23 16:15:49 -0400

    Refactor query_protocol

--- src/frontend/query_protocol.ml
+++ src/frontend/query_protocol.ml
@@ -56,15 +56,8 @@ module Compl = struct
       context : [ `Unknown | `Application of application_context ]
     }
 
-  type kind =
-    [ `Constructor
-    | `Labels
-    | `Modules
-    | `Modules_type
-    | `Types
-    | `Values
-    | `Variants
-    | `Keywords ]
+  module Kind = Completion_kind
+  type kind = Kind.t
 end
 
 type completions = Compl.t
@@ -133,53 +126,7 @@ type occurrences_status =
 
 type occurrence = { loc : Location.t; is_stale : bool }
 
-module Locate_context = struct
-  type t =
-    | Expr
-    | Module_path
-    | Module_type
-    | Patt
-    | Type
-    | Constant
-    | Constructor
-    | Label
-    | Unknown
-
-  let to_string = function
-    | Expr -> "expr"
-    | Module_path -> "module_path"
-    | Module_type -> "module_type"
-    | Patt -> "pattern"
-    | Type -> "type"
-    | Constant -> "constant"
-    | Constructor -> "constructor"
-    | Label -> "label"
-    | Unknown -> "unknown"
-
-  let of_string = function
-    | "expr" -> Some Expr
-    | "module_path" -> Some Module_path
-    | "module_type" -> Some Module_type
-    | "pattern" -> Some Patt
-    | "type" -> Some Type
-    | "constant" -> Some Constant
-    | "constructor" -> Some Constructor
-    | "label" -> Some Label
-    | "unknown" -> Some Unknown
-    | _ -> None
-
-  let all =
-    [ Expr;
-      Module_path;
-      Module_type;
-      Patt;
-      Type;
-      Constant;
-      Constructor;
-      Label;
-      Unknown
-    ]
-end
+module Locate_context = Locate_context
 
 type _ t =
   | Type_expr (* *) : string * Msource.position -> string t
