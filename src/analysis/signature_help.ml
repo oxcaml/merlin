a204731169f81a7c1fff3582ca84da2d27478884
Lucccyo <cha.git@mailo.fr>
2026-02-19 09:20:00 +0100

    Filter the parameters to not highlight parameters already written

--- src/analysis/signature_help.ml
+++ src/analysis/signature_help.ml
@@ -105,7 +105,8 @@ let separate_function_signature ~args (e : Typedtree.expression) =
       separate args ty2 ~parameters:(parameter :: parameters)
     | [], Tarrow ((label, _, _), ty1, ty2, _) ->
       let parameter =
-        print_parameter_offset ~arg:(Omitted omitted) ppf buffer e.exp_env label ty1
+        print_parameter_offset ~arg:(Omitted omitted) ppf buffer e.exp_env label
+          ty1
       in
       separate args ty2 ~parameters:(parameter :: parameters)
     (* end of function type, print remaining type without recording offsets *)
@@ -133,7 +134,8 @@ let first_unassigned_argument params =
     | _ -> false
   in
   let labelled = function
-    | { argument = Omitted _; label = Typedtree.Labelled _ | Optional _; _ } -> true
+    | { argument = Omitted _; label = Typedtree.Labelled _ | Optional _; _ } ->
+      true
     | _ -> false
   in
   try Some (List.index params ~f:positional)
@@ -149,16 +151,21 @@ let active_parameter_by_prefix ~prefix params =
       Some (String.common_prefix_len (Btype.prefixed_label_name l) prefix)
     | _ -> None
   in
-
+  let is_omitted = function
+    | Typedtree.Omitted _ -> true
+    | _ -> false
+  in
   let rec find_by_prefix ?(i = 0) ?longest_len ?longest_i = function
     | [] -> longest_i
-    | p :: ps -> (
+    | p :: ps when is_omitted p.argument -> (
+      (* The search is performed only on the arguments not already given in the parameters. *)
       match (common p.label, longest_len) with
       | Some common_len, Some longest_len when common_len > longest_len ->
         find_by_prefix ps ~i:(succ i) ~longest_len:common_len ~longest_i:i
       | Some common_len, None ->
         find_by_prefix ps ~i:(succ i) ~longest_len:common_len ~longest_i:i
       | _ -> find_by_prefix ps ~i:(succ i) ?longest_len ?longest_i)
+    | _ :: ps -> find_by_prefix ps ~i:(succ i) ?longest_len ?longest_i
   in
   find_by_prefix params
 
